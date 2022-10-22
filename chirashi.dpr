fd2086e3ecb94ba6a89b260bb8ea624b38a8d827
yhira <yhira@ma.mrr.jp>
2022-10-23 00:52:26 +0900

    64ビット化 二重起動防止

--- chirashi.dpr
+++ chirashi.dpr
@@ -2,12 +2,52 @@ program chirashi;
 
 uses
   Vcl.Forms,
+  Windows,
+  Dialogs,
+  Messages,
   Unit1 in 'Unit1.pas' {Form1},
   Unit2 in 'Unit2.pas' {Form2};
 
 {$R *.res}
 
+const
+  UniqueName:string = 'HogeHogeApp';
+
+//-----------------------------------------------------------------------------
+//  EnumWindows のコールバック関数
+//  起動済みのアプリを最前面に表示するために使用
+//
+//  lazarus-2.0.8 では ShowWindow(hWindow, SW_RESTORE) は機能しない
+//  代わりに WM_SYSCOMMAND で SC_RESTORE をポストしている
+//-----------------------------------------------------------------------------
+function EnumWndProc(hWindow: HWND; lData: LPARAM): BOOL;stdcall;
 begin
+  Result := True;
+  if GetProp(hWindow, PChar(lData)) = 1111 then begin
+    if IsIconic(hWindow) then begin
+      PostMessage(hWindow, WM_SYSCOMMAND, $FFF0 and SC_RESTORE, 0);
+    end;
+    SetForegroundWindow(hWindow);
+    Result := False;
+  end;
+end;
+
+//-----------------------------------------------------------------------------
+//  引数で指定したプロパティを持つウィンドウが起動しているかを確認する関数
+//-----------------------------------------------------------------------------
+function IsPrevAppExist(AName: string):Boolean;
+begin
+  Result := False;
+  CreateMutex(nil, True, PChar(AName));
+  if GetLastError = ERROR_ALREADY_EXISTS then begin
+    ShowMessage('このアプリは二重起動できません');
+    EnumWindows(@EnumWndProc, LPARAM(PChar(AName)));
+    Result := True;
+  end;
+end;
+begin
+  if IsPrevAppExist(UniqueName) then Exit;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
